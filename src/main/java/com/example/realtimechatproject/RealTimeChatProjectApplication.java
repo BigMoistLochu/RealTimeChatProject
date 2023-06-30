de6e14e168221edf939d9c7128c743958045004e
BigMoistLochu <krok.konrad1@gmail.com>
2023-07-01 01:00:15 +0200

    add project to git

---
+++ src/main/java/com/example/realtimechatproject/RealTimeChatProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.realtimechatproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeChatProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealTimeChatProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/realtimechatproject/RealTimeChatProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.realtimechatproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeChatProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
