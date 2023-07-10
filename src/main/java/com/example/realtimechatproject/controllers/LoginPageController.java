99147545f6bc5d808a7bae77216b543fb4340e0c
BigMoistLochu <krok.konrad1@gmail.com>
2023-07-10 05:40:48 +0200

    add LoginPageSecurity for validate form!

--- src/main/java/com/example/realtimechatproject/controllers/LoginPageController.java
+++ src/main/java/com/example/realtimechatproject/controllers/LoginPageController.java
@@ -1,6 +1,8 @@
 package com.example.realtimechatproject.controllers;
 
 import com.example.realtimechatproject.models.Form;
+import com.example.realtimechatproject.validationsForm.LoginPageSecurity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,22 +12,36 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class LoginPageController {
 
+    LoginPageSecurity loginPageSecurity;
+
+    @Autowired
+    public LoginPageController(LoginPageSecurity loginPageSecurity)
+    {
+        this.loginPageSecurity = loginPageSecurity;
+    }
 
     @GetMapping("/start")
     public String GetMainPage(Model model)
     {
-        //wrzucamy do login.html klase odpowiedzialna za formularz
+
         model.addAttribute("form", new Form());
         return "login.html";
     }
+
+
     @PostMapping("/form")
     public String GetFormPage(@ModelAttribute Form form)
     {
         // tutaj bedzie Validowany
-        System.out.println(form.getHaslo());
-        System.out.println(form.getLogin());
+        if(loginPageSecurity.checkForm(form))
 
-        return "index.html";
+            //po weryfikacji tego co przyszlo przechodzimy do sprawdzenia czy w bazie znajduje sie taki uzytkownik
+
+
+            return "index.html";
+        else
+        return "xddd";
+
     }
 
 
---
+++ src/main/java/com/example/realtimechatproject/validationsForm/LoginPageSecurity.java
@@ -0,0 +1,47 @@
+package com.example.realtimechatproject.validationsForm;
+
+
+import com.example.realtimechatproject.models.Form;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class LoginPageSecurity {
+
+    private Form form;
+
+
+    public boolean checkForm(Form form)
+    {
+        this.form = form;
+
+        if(checkLength()
+                && checkEmail())
+            return true;
+        else
+            return false;
+    }
+
+
+    private boolean checkLength()
+    {
+        if(form.getLogin().length()>0
+                && form.getHaslo().length()>0
+                && form.getLogin().length()<25
+                && form.getHaslo().length()<25)
+                return true;
+        else return false;
+
+    }
+
+    private boolean checkEmail()
+    {
+        if(!form.getLogin().contains("@"))
+            return false;
+        return true;
+    }
+
+
+
+
+}
