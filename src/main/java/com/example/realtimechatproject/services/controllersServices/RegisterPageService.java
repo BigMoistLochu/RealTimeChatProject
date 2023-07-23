19f3765cc4df6426cb9e0f00ebcd796bb14b2fef
BigMoistLochu <krok.konrad1@gmail.com>
2023-07-23 10:15:08 +0200

    add new validation in formFalidator class

--- src/main/java/com/example/realtimechatproject/services/controllersServices/RegisterPageService.java
+++ src/main/java/com/example/realtimechatproject/services/controllersServices/RegisterPageService.java
@@ -1,14 +1,28 @@
 package com.example.realtimechatproject.services.controllersServices;
 
 import com.example.realtimechatproject.models.LoginForm;
+import com.example.realtimechatproject.validationsForm.RegisterFormValidator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
 
+@Service
 public class RegisterPageService {
 
-    private LoginForm form;
 
-    public boolean checkForm(LoginForm form)
+    RegisterFormValidator registerFormValidator;
+
+    @Autowired
+    public RegisterPageService(RegisterFormValidator registerFormValidator) {
+        this.registerFormValidator = registerFormValidator;
+    }
+
+    public boolean getValidForm(LoginForm form, Model model)
     {
-        this.form = form;
+
+        if(registerFormValidator.checkRegisterForm(form)){
+            return false;
+        }
 
         return true;
     }
--- src/main/java/com/example/realtimechatproject/validationsForm/RegisterFormValidator.java
+++ src/main/java/com/example/realtimechatproject/validationsForm/RegisterFormValidator.java
@@ -1,9 +1,22 @@
 package com.example.realtimechatproject.validationsForm;
 
+import com.example.realtimechatproject.models.LoginForm;
+import org.springframework.stereotype.Component;
+
+@Component
 public class RegisterFormValidator {
 
+    private LoginForm form;
+
+    public boolean checkRegisterForm(LoginForm form)
+    {
+        this.form= form;
 
 
+
+
+        return true;
+    }
 
 
 
