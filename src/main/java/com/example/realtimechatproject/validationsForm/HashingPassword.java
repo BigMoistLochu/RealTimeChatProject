fcb1427b3461974e6fb92ff462f1548d9151db15
BigMoistLochu <krok.konrad1@gmail.com>
2023-07-26 21:40:08 +0200

    change validator, prepare HashingPassword class to hash all passwords

--- src/main/java/com/example/realtimechatproject/validationsForm/HashingPassword.java
+++ src/main/java/com/example/realtimechatproject/validationsForm/HashingPassword.java
@@ -1,13 +1,22 @@
 package com.example.realtimechatproject.validationsForm;
 
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class HashingPassword {
 
+    private String password = "haslo123";
 
+    public HashingPassword() throws NoSuchAlgorithmException {
+        MessageDigest messageDigest = MessageDigest.getInstance("MD5");
+        byte[] message = messageDigest.digest(password.getBytes());
+        BigInteger bigInteger = new BigInteger(1,message);
+        System.out.println(bigInteger.toString(16));
 
-
-
-
-
-
-    public HashingPassword(){}
+        if(bigInteger.toString(16).equals("75f3af6270ac666850054b12620f7442"))
+        {
+            System.out.println("poprawne");
+        }
+    }
 }
