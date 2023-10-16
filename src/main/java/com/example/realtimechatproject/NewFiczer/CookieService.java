eee3c21f9aea0d52cd359ea144fefb00f731c52d
BigMoistLochu <krok.konrad1@gmail.com>
2023-10-16 18:23:36 +0200

    add new method to the CookieService, wchich check the value from the cookie, if client send a request the method catch the request

--- src/main/java/com/example/realtimechatproject/NewFiczer/CookieService.java
+++ src/main/java/com/example/realtimechatproject/NewFiczer/CookieService.java
@@ -3,8 +3,15 @@ package com.example.realtimechatproject.NewFiczer;
 import com.example.realtimechatproject.services.restControllersServices.UserService;
 import com.example.realtimechatproject.validations.HashingEmail;
 import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
 @AllArgsConstructor
@@ -22,6 +29,18 @@ public class CookieService {
         }
 
         return new Cookie("ID_SESSION","notLogged");
+    }
+
+
+    public boolean checkCookie(HttpServletRequest request)
+    {
+        Map<String,String> mapOfCookie = new HashMap<>();
+        Arrays.stream(request.getCookies())
+                .map(cookie -> mapOfCookie.put(cookie.getName(),cookie.getValue()));
+        if(mapOfCookie.get("ID_SESSION").equals("notLogged"))
+            return false;
+
+        return true;
     }
 
 
